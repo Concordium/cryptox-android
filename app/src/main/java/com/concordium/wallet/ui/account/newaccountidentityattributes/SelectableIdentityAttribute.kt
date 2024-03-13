8d3ebb5132791e9e8cc78cdc265f55b5d38e020b
Oleg Koretsky <oko@concordium.com>
2024-03-13 17:53:45 +0200

    Remove ID attributes reveal from acc cration flow

--- app/src/main/java/com/concordium/wallet/ui/account/newaccountidentityattributes/SelectableIdentityAttribute.kt
+++
@@ -1,9 +0,0 @@
-package com.concordium.wallet.ui.account.newaccountidentityattributes
-
-import java.io.Serializable
-
-data class SelectableIdentityAttribute(
-    val name: String,
-    val value: String,
-    var isSelected: Boolean
-) : Serializable
