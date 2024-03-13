583cf1768eb30a9c3f8cf8b6dea997957a22df24
orhoj <jo@concordium.com>
2024-03-13 12:47:23 +0100

    Add special handling for age range and set statements

--- app/src/main/java/com/concordium/wallet/ui/walletconnect/DisplayStatements.kt
+++ app/src/main/java/com/concordium/wallet/ui/walletconnect/DisplayStatements.kt
@@ -14,6 +14,7 @@ import com.concordium.sdk.crypto.wallet.web3Id.Statement.NonMembershipStatement
 import com.concordium.sdk.crypto.wallet.web3Id.Statement.RangeStatement
 import com.concordium.sdk.crypto.wallet.web3Id.Statement.RequestStatement
 import com.concordium.sdk.crypto.wallet.web3Id.Statement.RevealStatement
+import com.concordium.sdk.crypto.wallet.web3Id.Statement.SetStatement
 import com.concordium.sdk.responses.accountinfo.credential.AttributeType
 import com.concordium.wallet.R
 import com.concordium.wallet.data.room.Identity
@@ -21,9 +22,16 @@ import com.concordium.wallet.data.util.IdentityAttributeConverterUtil
 import com.concordium.wallet.databinding.FragmentWalletConnectIdentityProofStatementCardBinding
 import com.concordium.wallet.databinding.FragmentWalletConnectIdentityProofStatementsBinding
 import com.concordium.wallet.databinding.IdentityProofStatementLineBinding
+import com.concordium.wallet.util.DateTimeUtil
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class DisplayStatements(context: Context, attrs: AttributeSet): LinearLayout(context, attrs) {
     var binding: FragmentWalletConnectIdentityProofStatementsBinding;
+
+    val MIN_DATE = "18000101"
+    val MAX_DATE = "99990101";
+    val EU_MEMBERS = listOf("AT", "BE", "BG", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE", "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "HR")
 
     init {
         binding = FragmentWalletConnectIdentityProofStatementsBinding.inflate(LayoutInflater.from(context))
@@ -101,14 +109,14 @@ class DisplayStatements(context: Context, attrs: AttributeSet): LinearLayout(con
 
         secretBinding.statementHeader.setText(R.string.zero_knowledge_proof)
         secretBinding.statementDescription.text = getStatementDescription(statement)
-        val statementLine = getStatementLine(getPropertyTitle(statement.attributeTag), getStatementValue(statement), provable)
+        val statementLine = getStatementLine(getPropertyTitle(statement), getStatementValue(statement), provable)
         secretBinding.statementLines.addView(statementLine)
 
         return secretBinding.root
     }
 
     private fun getStatementDescription(atomicStatement: AtomicStatement): String {
-        val name = getPropertyTitle(atomicStatement.attributeTag)
+        val name = IdentityAttributeConverterUtil.convertAttributeTag(context, atomicStatement.attributeTag)
         return when (atomicStatement) {
             is RangeStatement -> "This will prove that your $name is between ${getPropertyValue(atomicStatement.attributeTag, atomicStatement.lower.value)} and ${getPropertyValue(atomicStatement.attributeTag, atomicStatement.upper.value)}"
             is MembershipStatement -> "This will prove that your $name is 1 of the following: ${atomicStatement.set.joinToString { getPropertyValue(atomicStatement.attributeTag, it.value) }}"
@@ -117,23 +125,179 @@ class DisplayStatements(context: Context, attrs: AttributeSet): LinearLayout(con
         }
     }
 
-    private fun getPropertyTitle(attributeTag: String): String {
-        return IdentityAttributeConverterUtil.convertAttributeTag(context, attributeTag)
+    private fun getPropertyTitle(statement: AtomicStatement): String {
+        if (isAgeStatement(statement) && AttributeType.fromJSON(statement.attributeTag) == AttributeType.DOB) {
+            return context.getString(R.string.identity_proofs_age)
+        }
+        return IdentityAttributeConverterUtil.convertAttributeTag(context, statement.attributeTag)
     }
 
     private fun getPropertyValue(attributeTag: String, attributeValue: String): String {
         return IdentityAttributeConverterUtil.convertAttributeValue(context, attributeTag, attributeValue)
     }
 
-    private fun getStatementValue(atomicStatement: AtomicStatement?): String {
+    private fun addDays(date: String, days: Long): String {
+        val dateObject = DateTimeUtil.parseLongDate(date)
+            ?: throw RuntimeException("Failed to parse date: $date")
+        val dateWithDaysAdded = LocalDateTime.ofInstant(dateObject.toInstant(), ZoneId.systemDefault()).plusDays(days)
+        return dateToDateString(dateWithDaysAdded)
+    }
+
+    private fun dateToDateString(date: LocalDateTime): String {
+        val day = date.dayOfMonth.toString().padStart(2, '0')
+        val month = date.monthValue.toString().padStart(2, '0')
+        val year = date.year.toString()
+        return year + month + day
+    }
+
+    private fun isAgeStatement(statement: AtomicStatement): Boolean {
+        return when (statement) {
+            is RevealStatement -> false
+            is MembershipStatement -> false
+            is NonMembershipStatement -> false
+            is RangeStatement -> {
+                val today = getPastDate(0)
+                val isYearOffsetUpper = addDays(statement.upper.value, -1).substring(4) == today.substring(4)
+                if (statement.lower.value == MIN_DATE) {
+                    return isYearOffsetUpper
+                }
+
+                val isYearOffsetLower = addDays(statement.lower.value, -1).substring(4) == today.substring(4)
+                if (statement.upper.value > today) {
+                    return isYearOffsetLower
+                }
+
+                return isYearOffsetUpper && isYearOffsetLower
+            }
+            else -> false
+        }
+    }
+
+    private fun getYearFromDateString(date: String): Int {
+        return Integer.parseInt(date.substring(0, 4))
+    }
+
+    private fun formatDateString(date: String): String {
+        return date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6)
+    }
+
+    private fun getPastDate(yearsAgo: Long, daysOffset: Long = 0): String {
+        val now = LocalDateTime.now().minusYears(yearsAgo).minusDays(daysOffset)
+        return dateToDateString(now)
+    }
+
+    private fun getRangeStatementValue(rangeStatement: RangeStatement): String {
+        val lowerValue = rangeStatement.lower.value
+        val upperValue = rangeStatement.upper.value
+
+        when (AttributeType.fromJSON(rangeStatement.attributeTag)) {
+            AttributeType.DOB -> {
+                val today = getPastDate(0)
+
+                if (isAgeStatement(rangeStatement)) {
+                    val ageMin = getYearFromDateString(today) - getYearFromDateString(addDays(upperValue, -1))
+                    val ageMax = getYearFromDateString(today) - getYearFromDateString(addDays(lowerValue, -1)) - 1
+
+                    if (lowerValue == MIN_DATE) {
+                        return context.getString(R.string.identity_proofs_age_min, ageMin)
+                    }
+
+                    if (upperValue > today) {
+                        return context.getString(R.string.identity_proofs_age_max, ageMax)
+                    }
+
+                    if (ageMin == ageMax) {
+                        return context.getString(R.string.identity_proofs_age_exact, ageMin)
+                    }
+
+                    return context.getString(R.string.identity_proofs_age_between, ageMin, ageMax)
+                }
+
+                val minDateString = formatDateString(lowerValue)
+
+                val upper = if (today < upperValue) { today } else { upperValue }
+                val maxDateString = formatDateString(upper)
+
+                if (lowerValue == MIN_DATE) {
+                    return context.getString(R.string.identity_proofs_date_before, maxDateString)
+                }
+
+                if (upperValue > today) {
+                    return context.getString(R.string.identity_proofs_date_after_inclusive, minDateString)
+                }
+
+                return context.getString(R.string.identity_proofs_date_between, minDateString, maxDateString)
+            }
+            AttributeType.ID_DOC_ISSUED_AT, AttributeType.ID_DOC_EXPIRES_AT -> {
+                val minDateString = formatDateString(lowerValue)
+                val maxDateString = formatDateString(upperValue)
+
+                if (lowerValue == MIN_DATE) {
+                    return context.getString(R.string.identity_proofs_date_before, maxDateString)
+                }
+
+                if (upperValue == MAX_DATE) {
+                    return context.getString(R.string.identity_proofs_date_after_inclusive, minDateString)
+                }
+
+                return context.getString(R.string.identity_proofs_date_between, minDateString, maxDateString)
+            }
+            else -> {
+                return context.getString(R.string.identity_proofs_generic_between, lowerValue, upperValue)
+            }
+        }
+    }
+
+    private fun isEuCountrySet(countries: List<String>): Boolean {
+        return countries.size == EU_MEMBERS.size && countries.containsAll(EU_MEMBERS)
+    }
+
+    private fun getSetStatementValue(setStatement: SetStatement): String {
+        val setSize = setStatement.set.size
+        val isMembershipStatement: Boolean = when (setStatement) {
+            is MembershipStatement -> true
+            is NonMembershipStatement -> false
+            else -> throw IllegalArgumentException("An unsupported set statement was provided")
+        }
+
+        when (AttributeType.fromJSON(setStatement.attributeTag)) {
+            AttributeType.NATIONALITY, AttributeType.COUNTRY_OF_RESIDENCE -> {
+                if (isEuCountrySet(setStatement.set.map { it.value })) {
+                    return if (isMembershipStatement) context.getString(R.string.identity_proofs_eu_membership) else context.getString(R.string.identity_proofs_non_eu_membership)
+                }
+                return if (isMembershipStatement) context.getString(R.string.identity_proofs_countries, setSize) else context.getString(R.string.identity_proofs_none_countries, setSize)
+            }
+            AttributeType.ID_DOC_TYPE -> {
+                return if (isMembershipStatement) context.getString(R.string.identity_proofs_id_doc_types, setSize) else context.getString(R.string.identity_proofs_none_id_doc_types, setSize)
+            }
+            AttributeType.ID_DOC_ISSUER -> {
+                return if (isMembershipStatement) context.getString(R.string.identity_proofs_id_doc_issuer, setSize) else context.getString(R.string.identity_proofs_none_id_doc_issuer, setSize)
+            }
+            else -> {
+                return if (isMembershipStatement) context.getString(R.string.identity_proofs_set_fallback, setSize) else context.getString(R.string.identity_proofs_set_non_fallback, setSize)
+            }
+        }
+    }
+
+    private fun getStatementValue(atomicStatement: AtomicStatement): String {
         return when (atomicStatement) {
-            is RangeStatement -> "between ${getPropertyValue(atomicStatement.attributeTag,atomicStatement.lower.value)} and ${getPropertyValue(atomicStatement.attributeTag, atomicStatement.upper.value)}"
-            is MembershipStatement -> "1 of the following"
-            is NonMembershipStatement -> "none of the following"
+            is RangeStatement -> {
+                getRangeStatementValue(atomicStatement)
+            }
+
+            is MembershipStatement -> {
+                getSetStatementValue(atomicStatement)
+            }
+
+            is NonMembershipStatement -> {
+                getSetStatementValue(atomicStatement)
+            }
+
             else -> throw IllegalArgumentException("Unknown statement type")
         }
     }
 }
+
 
 /**
  * Get an IdentityObject compatible with the Concordium SDK methods.
