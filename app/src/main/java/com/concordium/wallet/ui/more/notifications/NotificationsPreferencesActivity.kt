deb55f9014319d84730f3049fcc7e6b188ca16a1
Oleg Koretsky <oko@concordium.com>
2024-06-24 17:17:41 +0300

    Request notification permission on the pref screen

--- app/src/main/java/com/concordium/wallet/ui/more/notifications/NotificationsPreferencesActivity.kt
+++ app/src/main/java/com/concordium/wallet/ui/more/notifications/NotificationsPreferencesActivity.kt
@@ -1,11 +1,17 @@
 package com.concordium.wallet.ui.more.notifications
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.concordium.wallet.R
 import com.concordium.wallet.databinding.ActivityNotificationsPreferencesBinding
 import com.concordium.wallet.ui.base.BaseActivity
+import com.concordium.wallet.util.Log
 
 class NotificationsPreferencesActivity : BaseActivity(
     R.layout.activity_notifications_preferences,
@@ -20,6 +26,14 @@ class NotificationsPreferencesActivity : BaseActivity(
             ViewModelProvider.AndroidViewModelFactory.getInstance(application)
         ).get()
     }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val notificationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+            Manifest.permission.POST_NOTIFICATIONS,
+            this::onNotificationPermissionResult
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +53,13 @@ class NotificationsPreferencesActivity : BaseActivity(
             this,
             binding.cis2TxSwitch::setChecked
         )
+        viewModel.requestNotificationPermissionLiveData.observe(this) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                notificationPermissionLauncher.launch(Unit)
+            } else {
+                Log.w("requesting_permission_on_unsupported_version")
+            }
+        }
     }
 
     private fun initViews() = with(binding) {
@@ -57,5 +78,12 @@ class NotificationsPreferencesActivity : BaseActivity(
         cis2TxSwitch.setOnClickListener {
             viewModel.onCis2TxClicked()
         }
+    }
+
+    private fun onNotificationPermissionResult(isGranted: Boolean) {
+        Log.d(
+            "received_result:" +
+                    "\nisGranted=$isGranted"
+        )
     }
 }
--- app/src/main/java/com/concordium/wallet/ui/more/notifications/NotificationsPreferencesViewModel.kt
+++ app/src/main/java/com/concordium/wallet/ui/more/notifications/NotificationsPreferencesViewModel.kt
@@ -1,10 +1,13 @@
 package com.concordium.wallet.ui.more.notifications
 
 import android.app.Application
+import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.concordium.wallet.core.arch.Event
 import com.concordium.wallet.data.preferences.NotificationsPreferences
+import com.concordium.wallet.util.Log
 
 class NotificationsPreferencesViewModel(application: Application) : AndroidViewModel(application) {
     private val notificationsPreferences = NotificationsPreferences(application)
@@ -15,6 +18,10 @@ class NotificationsPreferencesViewModel(application: Application) : AndroidViewM
     private val _areCis2TxNotificationsEnabledLiveData: MutableLiveData<Boolean> = MutableLiveData()
     val areCis2TxNotificationsEnabledLiveData: LiveData<Boolean> =
         _areCis2TxNotificationsEnabledLiveData
+    private val _requestNotificationPermissionLiveData: MutableLiveData<Event<Boolean>> =
+        MutableLiveData()
+    val requestNotificationPermissionLiveData: LiveData<Event<Boolean>> =
+        _requestNotificationPermissionLiveData
 
     init {
         _areCcdTxNotificationsEnabledLiveData.value =
@@ -27,11 +34,31 @@ class NotificationsPreferencesViewModel(application: Application) : AndroidViewM
         val areCcdTxNotificationsEnabled = _areCcdTxNotificationsEnabledLiveData.value != true
         _areCcdTxNotificationsEnabledLiveData.value = areCcdTxNotificationsEnabled
         notificationsPreferences.areCcdTxNotificationsEnabled = areCcdTxNotificationsEnabled
+        if (areCcdTxNotificationsEnabled) {
+            requestNotificationPermissionIfNeeded()
+        }
     }
 
     fun onCis2TxClicked() {
         val areCis2TxNotificationsEnabled = _areCis2TxNotificationsEnabledLiveData.value != true
         _areCis2TxNotificationsEnabledLiveData.value = areCis2TxNotificationsEnabled
         notificationsPreferences.areCis2TxNotificationsEnabled = areCis2TxNotificationsEnabled
+        if (areCis2TxNotificationsEnabled) {
+            requestNotificationPermissionIfNeeded()
+        }
+    }
+
+    private fun requestNotificationPermissionIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Log.d(
+                "requesting_permission"
+            )
+
+            _requestNotificationPermissionLiveData.postValue(Event(true))
+        } else {
+            Log.d(
+                "no_need_to_request"
+            )
+        }
     }
 }
