298bd4232d25967e25d45bd11b526fe132582472
zaiatsartem <az@concordium.com>
2024-12-13 16:48:58 +0200

    Change notification popup logic (#230)

--- app/src/main/java/com/concordium/wallet/ui/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/concordium/wallet/ui/welcome/WelcomeViewModel.kt
@@ -2,14 +2,29 @@ package com.concordium.wallet.ui.welcome
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.concordium.wallet.App
+import com.concordium.wallet.data.preferences.NotificationsPreferences
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class WelcomeViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val notificationsPreferences: NotificationsPreferences
+    private val _isNotificationDialogEverShowed = MutableStateFlow(true)
+    val isNotificationDialogEverShowed = _isNotificationDialogEverShowed.asStateFlow()
 
     val shouldSetUpPassword: Boolean
         get() = !App.appCore.session.hasSetupPassword
 
     init {
         App.appCore.session.startedInitialSetup()
+
+        notificationsPreferences = NotificationsPreferences(application)
+        viewModelScope.launch {
+            // Show notifications permission if never shown
+            _isNotificationDialogEverShowed.emit(notificationsPreferences.hasEverShownPermissionDialog)
+        }
     }
 }
