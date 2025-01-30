74b774567d565972754cecff39c1c3dd1e8ff69c
Oleg Koretsky <oko@concordium.com>
2025-01-30 14:33:55 +0200

    Remove validation/delegation fields from CreateTransferInput

--- app/src/main/java/com/concordium/wallet/data/cryptolib/CreateTransferInput.kt
+++ app/src/main/java/com/concordium/wallet/data/cryptolib/CreateTransferInput.kt
@@ -1,8 +1,6 @@
 package com.concordium.wallet.data.cryptolib
 
 import com.concordium.wallet.data.model.AccountData
-import com.concordium.wallet.data.model.BakerKeys
-import com.concordium.wallet.data.model.DelegationTarget
 import com.concordium.wallet.data.model.GlobalParams
 import com.concordium.wallet.data.model.InputEncryptedAmount
 import java.io.Serializable
@@ -20,13 +18,4 @@ data class CreateTransferInput(
     val receiverPublicKey: String?,
     val senderSecretKey: String?,
     val inputEncryptedAmount: InputEncryptedAmount?,
-    val capital: String?,
-    val restakeEarnings: Boolean? = null,
-    val delegationTarget: DelegationTarget? = null,
-    val metadataUrl: String? = null,
-    val openStatus: String? = null,
-    val bakerKeys: BakerKeys? = null,
-    val transactionFeeCommission: Double? = null,
-    val bakingRewardCommission: Double? = null,
-    val finalizationRewardCommission: Double? = null,
 ): Serializable
