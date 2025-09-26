e1a362ea82814722115db1ca5428188b4ab7a8dc
Oleg Koretsky <oko@concordium.com>
2025-09-26 13:20:50 +0300

    Do not cache repos in TokensInteractor
    Caching repos in long living TokensInteractor breaks wallet switching.

--- app/src/main/java/com/concordium/wallet/core/tokens/TokensInteractorModule.kt
+++ app/src/main/java/com/concordium/wallet/core/tokens/TokensInteractorModule.kt
@@ -1,9 +1,5 @@
 package com.concordium.wallet.core.tokens
 
-import com.concordium.wallet.App
-import com.concordium.wallet.data.AccountRepository
-import com.concordium.wallet.data.ContractTokensRepository
-import com.concordium.wallet.data.PLTRepository
 import com.concordium.wallet.data.backend.price.tokenPriceModule
 import com.concordium.wallet.data.backend.repository.ProxyRepository
 import org.koin.dsl.module
@@ -17,15 +13,6 @@ val tokensInteractorModule = module {
     single {
         TokensInteractor(
             proxyRepository = ProxyRepository(),
-            contractTokensRepository = ContractTokensRepository(
-                App.appCore.session.walletStorage.database.contractTokenDao()
-            ),
-            pltRepository = PLTRepository(
-                App.appCore.session.walletStorage.database.protocolLevelTokenDao()
-            ),
-            accountRepository = AccountRepository(
-                App.appCore.session.walletStorage.database.accountDao()
-            ),
             tokenPriceRepository = get(),
             loadTokensBalancesUseCase = LoadTokensBalancesUseCase(),
             loadCIS2TokensUseCase = LoadCIS2TokensUseCase(),
