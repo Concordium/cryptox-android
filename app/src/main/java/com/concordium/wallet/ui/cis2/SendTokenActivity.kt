03ababa8c805b813a699702515bb4c799ee2c79c
Oleg Koretsky <oko@concordium.com>
2025-10-28 17:04:21 +0200

    Fix the Send screen Max button
    Fix the Send screen Max button not working properly with large balances

--- app/src/main/java/com/concordium/wallet/ui/cis2/SendTokenActivity.kt
+++ app/src/main/java/com/concordium/wallet/ui/cis2/SendTokenActivity.kt
@@ -151,8 +151,9 @@ class SendTokenActivity : BaseActivity(R.layout.activity_send_token, R.string.ci
         binding.sendAllButton.setOnClickListener {
             binding.amount.setText(
                 CurrencyUtil.formatGTU(
-                    viewModel.sendTokenData.maxAmount ?: BigInteger.ZERO,
-                    viewModel.sendTokenData.token
+                    value = viewModel.sendTokenData.maxAmount ?: BigInteger.ZERO,
+                    token = viewModel.sendTokenData.token,
+                    withCommas = false,
                 )
             )
             enableSend()
