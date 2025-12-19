cc774032c57e95393d56e3343e6dbb343043867c
maryammajeed-cmd <mariam.majeed@concordium.com>
2025-12-19 10:41:08 +0500

    Automate Account create and account rename cases, Automate identity create and identity rename cases

--- cryptox-android-autotests/src/test/java/cryptox_AndroidTest/AccountCreation/AccountCreation.java
+++ cryptox-android-autotests/src/test/java/cryptox_AndroidTest/AccountCreation/AccountCreation.java
@@ -8,23 +8,59 @@ import java.net.MalformedURLException;
 import static config.appiumconnection.driver;
 import static config.appiumconnection.openAppiumSession;
 import static cryptox_AndroidTest.baseClass.*;
+import static pages.accountManagement.*;
 import static pages.createPassCodeScreen.createPassCodeNow;
+import static pages.generalMethods.*;
 import static pages.landingScreen.clickConnectButton;
 import static pages.landingScreen.clickGetStarted;
+import static pages.login.loginCryptoX;
 import static pages.popUps.AcceptNotificationPopUp;
 import static pages.repeatPassCodeScreen.repeatPassCodeNow;
+import static pages.verifyPIN.verifyPinAndPressOK;
 import static pages.walletAccountCreationScreen.activateAccount;
 
 public class AccountCreation {
-@Test
+    @Ignore
     public void createConcordionAccount() throws MalformedURLException {
-         Assert.assertTrue(AcceptNotificationPopUp());
-         Assert.assertTrue(clickConnectButton());
-         Assert.assertTrue(clickGetStarted());
-         Assert.assertTrue(activateAccount());
-         Assert.assertTrue(createPassCodeNow());
-         Assert.assertTrue(repeatPassCodeNow());
+        Assert.assertTrue(AcceptNotificationPopUp());
+        Assert.assertTrue(clickConnectButton());
+        Assert.assertTrue(clickGetStarted());
+        Assert.assertTrue(activateAccount());
+        Assert.assertTrue(createPassCodeNow());
+        Assert.assertTrue(repeatPassCodeNow());
+    }
 
+    @Test
+    public void Z_verify_if_a_user_can_create_an_account() {
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_account_btn_image", 10));
+        Assert.assertTrue(clickOnElement("create_account_button", 10));
+        Assert.assertTrue(SendTextToField("account_name_edittext", "AutomationAccount", 10));
+        Assert.assertTrue(clickOnElement("next_button", 10));
+        Assert.assertTrue(clickOnMostRecentIdentity());
+        Assert.assertTrue(clickOnElement("confirm_submit_button", 10));
+        Assert.assertTrue(verifyPinAndPressOK());
+        Assert.assertTrue(verifyTextById("account_name", "AutomationAccount", 20));
+    }
 
+    @Test
+    public void A_verify_if_user_can_rename_an_account() {
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_account_btn_image", 10));
+        Assert.assertTrue(clickOnElementByXpath("(//android.widget.ImageView[@content-desc=\"Account Settings\"])[1]", 10));
+        Assert.assertTrue(clickOnElement("change_name", 10));
+        Assert.assertTrue(SendTextToField("input_edittext", "AutomationTest", 10));
+        Assert.assertTrue(clickOnElement("android:id/button1", 10));
+        Assert.assertTrue(clickOnElement("toolbar_back_btn", 10));
+        Assert.assertTrue(verifyAccountName("AutomationTest", 20));
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_account_btn_image", 10));
+        Assert.assertTrue(clickOnAccount("AutomationTest",10));
     }
 }
---
+++ cryptox-android-autotests/src/test/java/cryptox_AndroidTest/AccountCreation/IdentityCreation.java
@@ -0,0 +1,59 @@
+package cryptox_AndroidTest.AccountCreation;
+
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+import static config.appiumconnection.driver;
+import static config.appiumconnection.log;
+import static cryptox_AndroidTest.baseClass.PackageName;
+import static pages.accountManagement.*;
+import static pages.appOperations.commands.performScroll;
+import static pages.generalMethods.*;
+import static pages.login.loginCryptoX;
+import static pages.verifyPIN.verifyPinAndPressOK;
+
+public class IdentityCreation {
+
+    @Test
+    public void Z_verify_if_a_user_can_create_new_identity() {
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("allow_button",5));
+        Assert.assertTrue(clickOnElement("toolbar_menu_drawer_btn", 10));
+        Assert.assertTrue(clickOnElement("identities_layout",10));
+        Assert.assertTrue(clickOnElement("toolbar_plus_btn_add_contact_image",10));
+        Assert.assertTrue(clickOnElementByXpath("//*[contains(@text,\"Generated IP 0\")]",30));
+        Assert.assertTrue(verifyPinAndPressOK());
+        Assert.assertTrue(WaitForElement("com.android.chrome:id/compositor_view_holder",20));
+        Assert.assertTrue(performScrollDown());
+        Assert.assertTrue(clickOnElementByXpath("//android.widget.Button[@text=\"Submit\"]",10));
+        Assert.assertTrue(clickOnElement("got_it_button",10));
+        Assert.assertTrue(clickOnElement("confirm_button",10));
+        Assert.assertTrue(clickOnElement("android:id/button2",10));
+        log.info("Successfully created new identity");
+    }
+
+    @Test
+    public void A_verify_if_user_can_rename_identity() {
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_menu_drawer_btn", 10));
+        Assert.assertTrue(clickOnElement("identities_layout",10));
+        Assert.assertTrue(clickOnMostRecentIdentity());
+        Assert.assertTrue(clickOnElement("name_icon",10));
+        Assert.assertTrue(SendTextToField("input_edittext", "AutomationTest", 20));
+        Assert.assertTrue(clickOnElement("android:id/button1",20));
+        Assert.assertTrue(verifyTextById("name_textview","AutomationTest",10));
+        Assert.assertTrue(clickOnElement("toolbar_back_btn",10));
+        Assert.assertTrue(verifyMostRecentIdentityName("AutomationTest",20));
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_menu_drawer_btn", 10));
+        Assert.assertTrue(clickOnElement("identities_layout",10));
+        Assert.assertTrue(verifyMostRecentIdentityName("AutomationTest",20));
+    }
+}
