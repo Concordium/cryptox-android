4d21345175c54c8ec5a25cfa2ef2637ce4139f73
maryammajeed-cmd <mariam.majeed@concordium.com>
2026-02-06 13:13:33 +0500

    fix-pipeline-ui-fixes-updated

--- cryptox-android-autotests/src/test/java/cryptox_AndroidTest/AccountCreation/AccountCreation.java
+++ cryptox-android-autotests/src/test/java/cryptox_AndroidTest/AccountCreation/AccountCreation.java
@@ -1,5 +1,6 @@
 package cryptox_AndroidTest.AccountCreation;
 
+import config.RetryAnalyzer;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -8,23 +9,51 @@ import java.net.MalformedURLException;
 import static config.appiumconnection.driver;
 import static config.appiumconnection.openAppiumSession;
 import static cryptox_AndroidTest.baseClass.*;
+import static pages.accountManagement.*;
 import static pages.createPassCodeScreen.createPassCodeNow;
+import static pages.generalMethods.*;
 import static pages.landingScreen.clickConnectButton;
 import static pages.landingScreen.clickGetStarted;
+import static pages.login.loginCryptoX;
 import static pages.popUps.AcceptNotificationPopUp;
 import static pages.repeatPassCodeScreen.repeatPassCodeNow;
+import static pages.verifyPIN.verifyPinAndPressOK;
 import static pages.walletAccountCreationScreen.activateAccount;
 
 public class AccountCreation {
-@Test
-    public void createConcordionAccount() throws MalformedURLException {
-         Assert.assertTrue(AcceptNotificationPopUp());
-         Assert.assertTrue(clickConnectButton());
-         Assert.assertTrue(clickGetStarted());
-         Assert.assertTrue(activateAccount());
-         Assert.assertTrue(createPassCodeNow());
-         Assert.assertTrue(repeatPassCodeNow());
 
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void Z_verify_if_a_user_can_create_an_account() {
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_account_btn_image", 20));
+        Assert.assertTrue(clickOnElement("create_account_button", 20));
+        Assert.assertTrue(SendTextToField("account_name_edittext", "AutomationAccount", 20));
+        Assert.assertTrue(clickOnElement("next_button", 20));
+        Assert.assertTrue(clickOnMostRecentIdentity());
+        Assert.assertTrue(clickOnElement("confirm_submit_button", 20));
+        Assert.assertTrue(verifyPinAndPressOK());
+        Assert.assertTrue(verifyTextById("account_name", "AutomationAccount", 20));
+    }
 
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+    public void A_verify_if_user_can_rename_an_account() {
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_account_btn_image", 20));
+        Assert.assertTrue(clickOnElementByXpath("(//android.widget.ImageView[@content-desc=\"Account Settings\"])[1]", 20));
+        Assert.assertTrue(clickOnElement("change_name", 20));
+        Assert.assertTrue(SendTextToField("input_edittext", "AutomationTest", 20));
+        Assert.assertTrue(clickOnElement("android:id/button1", 20));
+        Assert.assertTrue(clickOnElement("toolbar_back_btn", 20));
+        Assert.assertTrue(verifyAccountName("AutomationTest", 20));
+        driver.terminateApp(PackageName);
+        driver.activateApp(PackageName);
+        Assert.assertTrue(loginCryptoX());
+        Assert.assertTrue(clickOnElement("toolbar_account_btn_image", 20));
+        Assert.assertTrue(clickOnAccount("AutomationTest",20));
     }
 }
+
