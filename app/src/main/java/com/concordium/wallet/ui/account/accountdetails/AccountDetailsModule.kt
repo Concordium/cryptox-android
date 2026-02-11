9c2b556729ae8f648590c2e591b457e882d1e978
Artem Zaiats <az@concordium.com>
2026-02-11 11:56:10 +0200

    Update deprecated DI functions

--- app/src/main/java/com/concordium/wallet/ui/account/accountdetails/AccountDetailsModule.kt
+++ app/src/main/java/com/concordium/wallet/ui/account/accountdetails/AccountDetailsModule.kt
@@ -3,7 +3,7 @@ package com.concordium.wallet.ui.account.accountdetails
 import com.concordium.wallet.ui.MainViewModel
 import com.concordium.wallet.ui.account.accountsoverview.AccountBalanceViewModel
 import org.koin.android.ext.koin.androidApplication
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val accountDetailsModule = module {
--- app/src/main/java/com/concordium/wallet/ui/cis2/TokensListModule.kt
+++ app/src/main/java/com/concordium/wallet/ui/cis2/TokensListModule.kt
@@ -2,7 +2,7 @@ package com.concordium.wallet.ui.cis2
 
 import com.concordium.wallet.core.tokens.tokensInteractorModule
 import com.concordium.wallet.ui.account.accountdetails.AccountDetailsViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val tokensListModule = module {
