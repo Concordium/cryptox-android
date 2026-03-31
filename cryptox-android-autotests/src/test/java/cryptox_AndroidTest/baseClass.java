a7c61679fb388743fc767371eefa3fee8984f97f
maryammajeed-cmd <maryam.majeed@concordium.com>
2026-03-31 17:57:30 +0500

    Custom network (#355)
    * Added custom network switch method
    
    * changes in yml file

--- cryptox-android-autotests/src/test/java/cryptox_AndroidTest/baseClass.java
+++ cryptox-android-autotests/src/test/java/cryptox_AndroidTest/baseClass.java
@@ -1,11 +1,18 @@
 package cryptox_AndroidTest;
 
 import config.appiumconnection;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 import java.net.MalformedURLException;
 import static config.appiumconnection.*;
 import static config.systemInfo.getDeviceDimensions;
+import static pages.generalMethods.*;
+
 import config.configLoader;
 
 public class baseClass {
@@ -17,7 +24,7 @@ public static String EmulatorCloud = "emulator-5554"; // Change to your device I
 public static String appURL;
 public static String Device;
 public static String testnetPackageName = "com.pioneeringtechventures.wallet.testnet";
-public static String stagePackageName = "com.pioneeringtechventures.wallet.stagenet";
+public static String stagePackageName = "com.pioneeringtechventures.wallet.testnet"; //"com.pioneeringtechventures.wallet.stagenet";
 public static String activityName = "com.concordium.wallet.ui.MainActivity";
 public static String PackageName = stagePackageName;
 
@@ -39,18 +46,66 @@ public static String PackageName = stagePackageName;
 
         openAppiumSession(Device, PackageName, activityName);
         getDeviceDimensions();
+        getCustomNetwork();
     }
 
     @AfterTest
     public void tearDown(){
 ////    driver.removeApp(PackageName); // This will remove the app from the device
 //}
-        if(!localExecution){
-            driver.removeApp(PackageName); // This will remove the app from the device
-        }
-        else {
+//        if(!localExecution){
+//            driver.removeApp(PackageName); // This will remove the app from the device
+//        }
+//        else {
             driver.quit(); // This will close the appium driver.
+//        }
+    }
+
+    public void getCustomNetwork() throws InterruptedException {
+        driver.activateApp(PackageName);
+        if (isElementPresent("toolbar_networks_btn", 5)) {
+            clickOnElement("toolbar_networks_btn", 20);
+            turnOnToggle("dev_mode_switch", 20);
+            clickOnElement("add_button", 20);
+            SendTextToFieldByXpath("(//android.widget.EditText[@resource-id=\"com.pioneeringtechventures.wallet.testnet:id/edittext\"])[1]", "Stagenet", 20);
+            SendTextToFieldByXpath("(//android.widget.EditText[@resource-id=\"com.pioneeringtechventures.wallet.testnet:id/edittext\"])[2]", "https://wallet-proxy.stagenet.concordium.com/", 20);
+            SendTextToFieldByXpath("(//android.widget.EditText[@resource-id=\"com.pioneeringtechventures.wallet.testnet:id/edittext\"])[4]", "https://stagenet.ccdscan.io/", 20);
+            WebDriverWait wait = new WebDriverWait(driver, 20);
+            MobileElement saveBtn = (MobileElement) wait.until(ExpectedConditions.elementToBeClickable(By.id("save_button")));
+            saveBtn.click();
+            saveBtn.click();
+            Thread.sleep(3000);
+            Assert.assertTrue(verifyElementByXpath("//android.widget.TextView[@resource-id=\"com.pioneeringtechventures.wallet.testnet:id/name_text_view\" and @text=\"Stagenet\"]", 20));
+        } else {
+            System.out.println("toolbar_networks_btn not visible — skipping custom network setup.");
         }
     }
+    public boolean turnOnToggle(String elementId, int timeoutInSeconds) {
+        try {
+            MobileElement toggle = (MobileElement) waitForElement(By.id(elementId), timeoutInSeconds);
+            if (toggle.getAttribute("checked").equals("false")) {
+                toggle.click();
+            }
+            log.info("Successfully Toggled");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Unable to perform Toggled");
+            return false;
+        }
+    }
+
+    public boolean isElementPresent(String locatorKey, int timeout) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(locatorKey)));
+            log.info("Network button {} is visible. Proceeding with the flow of setting a customized network.", locatorKey);
+            return true;
+        } catch (Exception e) {
+            log.error("Network button {} is not visible. Proceeding with the normal flow without setting a customized network.", locatorKey);
+            return false;
+        }
+    }
+
 }
 
